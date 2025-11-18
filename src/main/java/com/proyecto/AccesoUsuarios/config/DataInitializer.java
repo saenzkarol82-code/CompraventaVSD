133e133f36aed1cd72c9e7cde1c148a88831aea9
karol saenz <karolsaenz82@gmail.com>
2025-11-18 01:54:54 -0500

    version del proyecto , esta al 100% de la visualizacion del proyecto funcionana las capas de seguridad y accede, correctamente

--- src/main/java/com/proyecto/AccesoUsuarios/config/DataInitializer.java
+++ src/main/java/com/proyecto/AccesoUsuarios/config/DataInitializer.java
@@ -12,27 +12,37 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration  //comandLineRunner comoponente que se ejecuta automaticamente al arrancar la aplicacion.
 public class DataInitializer {
      @Bean
-public CommandLineRunner initDatabase(UsuarioRepository usuarioRepo) {
-    return args -> {
+     public CommandLineRunner initDatabase(UsuarioRepository usuarioRepo) {
+     return args -> {
         // Usuario ADMIN
         if (usuarioRepo.findByCorreoUsuario("admin@correo.com").isEmpty()) {
-            Usuario admin = new Usuario();
-            admin.setCorreoUsuario("admin@correo.com");
-            admin.setPassword(new BCryptPasswordEncoder().encode("123"));
-            admin.setRol("ADMIN");
-            usuarioRepo.save(admin);
-            System.out.println("✅ Usuario admin creado con éxito");
-        } else {
-                System.out.println("ℹ️ Usuario admin ya existe");
+           Usuario admin = new Usuario();
+           admin.setDocumentoUsuario("1000522375");
+           admin.setNombreUsuario("Karol");
+           admin.setApellidoUsuario("Saenz");
+           admin.setTelefonoUsuario(302869303);
+           admin.setCorreoUsuario("adminkarol@correo.com");
+           admin.setPassword(new BCryptPasswordEncoder().encode("123"));
+           admin.setRol("ADMIN");
+           usuarioRepo.save(admin);
+
+           System.out.println("✅ Usuario admin creado con éxito");
+        }else{
+           System.out.println("ℹ️ Usuario admin ya existe");
         }
 
         // Usuario CLIENTE
         if (usuarioRepo.findByCorreoUsuario("cliente@correo.com").isEmpty()) {
             Usuario cliente = new Usuario();
+            cliente.setDocumentoUsuario("0002");
+            cliente.setNombreUsuario("Cliente");
+            cliente.setApellidoUsuario("Ejemplo");
+            cliente.setTelefonoUsuario(321987654);
             cliente.setCorreoUsuario("cliente@correo.com");
             cliente.setPassword(new BCryptPasswordEncoder().encode("123"));
             cliente.setRol("CLIENTE");
             usuarioRepo.save(cliente);
+
             System.out.println("✅ Usuario cliente creado con éxito");
         } else {
                 System.out.println("ℹ️ Usuario cliente ya existe");
@@ -41,10 +51,15 @@ public CommandLineRunner initDatabase(UsuarioRepository usuarioRepo) {
         // Usuario VENDEDOR
         if (usuarioRepo.findByCorreoUsuario("vendedor@correo.com").isEmpty()) {
             Usuario vendedor = new Usuario();
+            vendedor.setDocumentoUsuario("0003");
+            vendedor.setNombreUsuario("Vendedor");
+            vendedor.setApellidoUsuario("Oficial");
+            vendedor.setTelefonoUsuario(301987654);
             vendedor.setCorreoUsuario("vendedor@correo.com");
             vendedor.setPassword(new BCryptPasswordEncoder().encode("123"));
             vendedor.setRol("VENDEDOR");
             usuarioRepo.save(vendedor);
+
             System.out.println("✅ Usuario vendedor creado con éxito");
         }
          else {
